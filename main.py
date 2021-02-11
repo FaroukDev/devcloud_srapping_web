f78165d0305ffb040ff7d511f59eb43f1b83588d
FaroukDev <fa.elayache1@gmail.com>
2021-02-11 16:21:30 +0100

    add my_dict into table games

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import urllib.error
-import ssl
-
+import ssl 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-#url of scrapping debut du script
-urlpage = 'https://www.jeuxvideo.com/meilleurs/machine-30/'
+def getXBox():
+    #url of scrapping debut du script
+    urlpage = 'https://www.jeuxvideo.com/meilleurs/machine-30/'
 
-#on request la page avec urllib
-page = urllib.request.urlopen(urlpage)
-soup = BeautifulSoup(page, 'html.parser')
-#print(soup.prettify())
+    #on request la page avec urllib
+    page = urllib.request.urlopen(urlpage)
+    soup = BeautifulSoup(page, 'html.parser')
+    #print(soup.prettify())
 
-#scrap element a in xbox page
-title = soup.find_all('a', attrs={'class': 'gameTitleLink__196nPy'})
-#results = table.find_all('tr')
+    #scrap element a in xbox page
+    title = soup.find_all('a', attrs={'class': 'gameTitleLink__196nPy'})
+    #results = table.find_all('tr')
 
-#for loop in element a of xbox page
-for i in range(0,5):
-#print('Number of results', title[0])
-    title_slice = title[i].text
-    print(title_slice[0:-8])
-
+    games_dict = {}
+    #for loop in element a of xbox page
+    for i in range(0,5):
+    #print('Number of results', title[0])
+        title_slice = title[i].text
+        print(title_slice[0:-8])
+        games_dict.update({i+1:title_slice})
+        print(games_dict)
+    return games_dict
 
 
 
--- msql_query.py
+++ msql_query.py
@@ -1,4 +1,7 @@
 import mysql.connector
+from main import getXBox
+
+my_dict = getXBox()
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -10,15 +13,21 @@ mydb = mysql.connector.connect(
 print('cest good:',mydb)
 
 
-def addGames():
-    mycursor = mydb.cursor()
-    mycursor.execute("DROP TABLE IF EXISTS games", multi=True)
-    mycursor.execute("CREATE TABLE IF NOT EXISTS games(games_id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(255), note int NOT NULL)")
-    mySql_insert_query = """INSERT INTO games(name, note) VALUES('assassins screed', 6.8)"""
-    cursor = mydb.cursor()
-    mycursor.execute(mySql_insert_query)
-    mydb.commit()
-    print(cursor.rowcount,"Record inserted successfully into games table")
-    cursor.close()
+def addGames(games, mycursor,games_dict):
+    my_cursor = mycursor.cursor()
+    my_cursor.execute("DROP TABLE IF EXISTS games", multi=True)
+    my_cursor.execute("CREATE TABLE IF NOT EXISTS games(games_id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(255))")
+    #mySql_insert_query = """INSERT INTO games(name, note) VALUES('assassins screed', 6.8)"""
+    #cursor = mydb.cursor()
+    #mycursor.execute(mySql_insert_query)
+    #mydb.commit()
+    #print(cursor.rowcount,"Record inserted successfully into games table")
+    #cursor.close()
+    for i in range (1,6):
+            mySql_insert_query = f"""INSERT INTO {games} (name) VALUES ('{games_dict[i]}')"""
+            my_cursor.execute(mySql_insert_query)  
+            mycursor.commit()
+    
+addGames("games",mydb,my_dict)
 
-addGames()
+
