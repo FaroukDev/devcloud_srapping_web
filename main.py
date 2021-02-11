c0ed382845678db7fa1b6200d63ba27a51434a61
FaroukDev <fa.elayache1@gmail.com>
2021-02-11 17:20:18 +0100

    add function insertGames in msql_querie

--- main.py
+++ main.py
@@ -22,10 +22,66 @@ def getXBox():
     for i in range(0,5):
     #print('Number of results', title[0])
         title_slice = title[i].text
-        print(title_slice[0:-8])
+        title_slice[0:-8]
+        title_slice = title_slice.replace("'"," ")
         games_dict.update({i+1:title_slice})
-        print(games_dict)
     return games_dict
+
+print(getXBox())
+def getPs5():
+    #url of scrapping debut du script
+    urlpage = 'https://www.jeuxvideo.com/meilleurs/machine-22/'
+
+    #on request la page avec urllib
+    page = urllib.request.urlopen(urlpage)
+    soup = BeautifulSoup(page, 'html.parser')
+    #print(soup.prettify())
+
+    #scrap element a in xbox page
+    title = soup.find_all('a', attrs={'class': 'gameTitleLink__196nPy'})
+    #results = table.find_all('tr')
+
+    games_dict = {}
+    #for loop in element a of xbox page
+    for i in range(0,5):
+    #print('Number of results', title[0])
+        title_slice = title[i].text
+        title_slice[0:-8]
+        title_slice = title_slice.replace("'"," ")
+        games_dict.update({i+1:title_slice})
+    return games_dict
+
+print(getPs5())
+
+def getPc():
+    #url of scrapping debut du script
+    urlpage = 'https://www.jeuxvideo.com/meilleurs/machine-10/'
+
+    #on request la page avec urllib
+    page = urllib.request.urlopen(urlpage)
+    soup = BeautifulSoup(page, 'html.parser')
+    #print(soup.prettify())
+
+    #scrap element a in xbox page
+    title = soup.find_all('a', attrs={'class': 'gameTitleLink__196nPy'})
+    #results = table.find_all('tr')
+
+    games_dict = {}
+    #for loop in element a of xbox page
+    for i in range(0,5):
+    #print('Number of results', title[0])
+        title_slice = title[i].text
+        title_slice = title_slice[0:-8]
+        title_slice = title_slice.replace("'"," ")
+        games_dict.update({i+1:title_slice})
+    return games_dict
+
+print(getPc())
+
+  
+
+    
+
 
 
 
--- msql_query.py
+++ msql_query.py
@@ -1,7 +1,9 @@
 import mysql.connector
-from main import getXBox
+from main import getXBox, getPs5, getPc
 
 my_dict = getXBox()
+my_dict1 = getPs5()
+my_dict2 = getPc()
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -10,24 +12,32 @@ mydb = mysql.connector.connect(
   database="video_game"
 )
 
-print('cest good:',mydb)
-
-
 def addGames(games, mycursor,games_dict):
     my_cursor = mycursor.cursor()
     my_cursor.execute("DROP TABLE IF EXISTS games", multi=True)
+    my_cursor.execute("DROP TABLE IF EXISTS ps5", multi=True)
+    my_cursor.execute("DROP TABLE IF EXISTS pc", multi=True)
     my_cursor.execute("CREATE TABLE IF NOT EXISTS games(games_id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(255))")
+    my_cursor.execute("CREATE TABLE IF NOT EXISTS ps5(games_id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(255))")
+    my_cursor.execute("CREATE TABLE IF NOT EXISTS pc(games_id int PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(255))")
     #mySql_insert_query = """INSERT INTO games(name, note) VALUES('assassins screed', 6.8)"""
     #cursor = mydb.cursor()
     #mycursor.execute(mySql_insert_query)
     #mydb.commit()
     #print(cursor.rowcount,"Record inserted successfully into games table")
     #cursor.close()
-    for i in range (1,6):
-            mySql_insert_query = f"""INSERT INTO {games} (name) VALUES ('{games_dict[i]}')"""
-            my_cursor.execute(mySql_insert_query)  
-            mycursor.commit()
     
 addGames("games",mydb,my_dict)
+addGames("ps5",mydb,my_dict1)
+addGames("pc",mydb,my_dict2)
 
+def insertGames(games,mycursor,games_dict):
+    my_cursor = mycursor.cursor()
+    for i in range (1,6):
+                mySql_insert_query = f"""INSERT INTO {games} (name) VALUES ('{games_dict[i]}')"""
+                my_cursor.execute(mySql_insert_query)  
+                mycursor.commit()
 
+insertGames("games",mydb,my_dict)
+insertGames("ps5",mydb,my_dict1)
+insertGames("pc",mydb,my_dict2)
